681a2fa56aa8fd2eaaf398986f72aa7954b66531
Macharia <machariaa340@gmail.com>
2023-05-18 21:17:49 +0300

    fits commit

---
+++ store/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from . import views
+
+from store.controller import authview, cart, checkout
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('collections', views.collections, name="collections"),
+    path('collections/<str:slug>', views.collectionsview, name="collectionsview"),
+    path('collections/<str:cate_slug>/<str:prod_slug>', views.productview, name="productview"),
+
+    path('register/', authview.register, name="register" ),
+    path('login/', authview.loginpage, name="loginpage"),
+    path('logout/', authview.logoutpage, name="logout"),
+
+    path('add-to-cart', cart.addtocart, name="addtocart"),
+    path('cart', cart.viewcart, name="cart"),
+    path('update-cart', cart.updatecart, name="updatecart" ),
+    path('delete-cart-item', cart.deletecartitem, name="deletecartitem"),
+
+    
+    path('checkout', checkout.index, name="checkout"),
+    path('place-order', checkout.placeorder, name="placeorder"),
+
+    
+]
